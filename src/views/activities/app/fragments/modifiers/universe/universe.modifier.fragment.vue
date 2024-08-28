e358afd8bf24f29796bf3b7b5758be83bccfe0d0
Time Kadel <time.kadel@gmail.com>
2024-08-28 17:53:13 +0200

    refactor(views/activities/app/fragmants/modifiers/universe): select default fixture on load/reload

--- src/views/activities/app/fragments/modifiers/universe/universe.modifier.fragment.vue
+++ src/views/activities/app/fragments/modifiers/universe/universe.modifier.fragment.vue
@@ -139,6 +139,7 @@ export default {
       if (id !== undefined) {
         try {
           this.universe = this.$show.universePool.getFromId(id);
+          this.selectFixture(0);
         } catch (err) {
           this.universe = {};
         }
